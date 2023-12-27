b9c61ca0acc0ddab8c5ce09810fc4e0e12e2309c
57naveen <naveenkishore65@gmail.com>
2023-12-27 18:07:50 +0530

    Working on bookinventory

--- ElibraryManagementWeb/adminbookinventory.aspx
+++ ElibraryManagementWeb/adminbookinventory.aspx
@@ -224,19 +224,13 @@
                         <asp:GridView class="table table-striped table-bordered" ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="book_id" DataSourceID="SqlDataSource1">
                             <Columns>
                                 <asp:BoundField DataField="book_id" HeaderText="book_id" ReadOnly="True" SortExpression="book_id" />
-                                <asp:BoundField DataField="book_name" HeaderText="book_name" SortExpression="book_name" />
-                                <asp:BoundField DataField="genre" HeaderText="genre" SortExpression="genre" />
-                                <asp:BoundField DataField="author_name" HeaderText="author_name" SortExpression="author_name" />
-                                <asp:BoundField DataField="publisher_name" HeaderText="publisher_name" SortExpression="publisher_name" />
-                                <asp:BoundField DataField="publish_date" HeaderText="publish_date" SortExpression="publish_date" />
-                                <asp:BoundField DataField="language" HeaderText="language" SortExpression="language" />
-                                <asp:BoundField DataField="edition" HeaderText="edition" SortExpression="edition" />
-                                <asp:BoundField DataField="book_cost" HeaderText="book_cost" SortExpression="book_cost" />
-                                <asp:BoundField DataField="no_of_pages" HeaderText="no_of_pages" SortExpression="no_of_pages" />
-                                <asp:BoundField DataField="book_description" HeaderText="book_description" SortExpression="book_description" />
-                                <asp:BoundField DataField="actual_stock" HeaderText="actual_stock" SortExpression="actual_stock" />
-                                <asp:BoundField DataField="current_stock" HeaderText="current_stock" SortExpression="current_stock" />
-                                <asp:BoundField DataField="book_img_link" HeaderText="book_img_link" SortExpression="book_img_link" />
+                               
+                                <asp:TemplateField>
+                                    <ItemTemplate>
+                                      
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                               
                             </Columns>
                          </asp:GridView>
                      </div>
--- ElibraryManagementWeb/adminbookinventory.aspx.cs
+++ ElibraryManagementWeb/adminbookinventory.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -15,7 +16,7 @@ namespace ElibraryManagementWeb
 
         string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
-        {   
+        {
 
             //This function is used to refersh the data from the database
             GridView1.DataBind();
@@ -69,7 +70,7 @@ namespace ElibraryManagementWeb
                     con.Open();
                 }
 
-                SqlCommand cmd = new SqlCommand("select*from book_master_tbl where book_id='" + TextBox1.Text.Trim() + "' OR book_name ='"+TextBox2.Text.Trim()+"' ", con);
+                SqlCommand cmd = new SqlCommand("select*from book_master_tbl where book_id='" + TextBox1.Text.Trim() + "' OR book_name ='" + TextBox2.Text.Trim() + "' ", con);
 
                 SqlDataAdapter da = new SqlDataAdapter(cmd);
 
@@ -104,19 +105,19 @@ namespace ElibraryManagementWeb
 
                 if (con.State == ConnectionState.Closed)
                 {
-                    con .Open();
+                    con.Open();
                 }
 
                 SqlCommand cmd = new SqlCommand("select author_name from author_master_tbl", con);
                 SqlDataAdapter da = new SqlDataAdapter(cmd);
-                DataTable dt = new DataTable(); 
+                DataTable dt = new DataTable();
                 da.Fill(dt);
                 DropDownList3.DataSource = dt;
                 DropDownList3.DataValueField = "author_name";
                 DropDownList3.DataBind();
 
                 cmd = new SqlCommand("select publisher_name from publisher_master_tbl", con);
-                 da = new SqlDataAdapter(cmd);
+                da = new SqlDataAdapter(cmd);
                 dt = new DataTable();
                 da.Fill(dt);
                 DropDownList2.DataSource = dt;
@@ -125,23 +126,74 @@ namespace ElibraryManagementWeb
 
 
 
-
-
-
-
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
 
             }
-            catch { 
-
-            }   
         }
 
         void addNewBook()
         {
-             
+            try
+            {
+                string genres = "";
+                foreach (int i in ListBox1.GetSelectedIndices())
+                {
+                    genres = genres + ListBox1.Items[i] + ",";
+                }
+                // genres = Adventure,Self Help,
+                genres = genres.Remove(genres.Length - 1);
 
+                string filepath = "~/book_inventory/books1.png";
+                string filename = Path.GetFileName(FileUpload1.PostedFile.FileName);
+                FileUpload1.SaveAs(Server.MapPath("book_inventory/" + filename));
+                filepath = "~/book_inventory/" + filename;
+
+
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO book_master_tbl(book_id,book_name,genre,author_name,publisher_name,publish_date,language,edition,book_cost,no_of_pages,book_description,actual_stock,current_stock,book_img_link) values(@book_id,@book_name,@genre,@author_name,@publisher_name,@publish_date,@language,@edition,@book_cost,@no_of_pages,@book_description,@actual_stock,@current_stock,@book_img_link)", con);
+
+                cmd.Parameters.AddWithValue("@book_id", TextBox1.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_name", TextBox2.Text.Trim());
+                cmd.Parameters.AddWithValue("@genre", genres);
+                cmd.Parameters.AddWithValue("@author_name", DropDownList3.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@publisher_name", DropDownList2.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@publish_date", TextBox3.Text.Trim());
+                cmd.Parameters.AddWithValue("@language", DropDownList1.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@edition", TextBox9.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_cost", TextBox10.Text.Trim());
+                cmd.Parameters.AddWithValue("@no_of_pages", TextBox11.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_description", TextBox6.Text.Trim());
+                cmd.Parameters.AddWithValue("@actual_stock", TextBox4.Text.Trim());
+                cmd.Parameters.AddWithValue("@current_stock", TextBox4.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_img_link", filepath);
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Book added successfully.');</script>");
+                GridView1.DataBind();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
         }
 
 
+
+
+
+
+
     }
+
+
 }
